ba7e3d89cc05eafb19e2821e49acfb2a29d2b817
Joanna Sobczak <joanna.sobczak@vp.pl>
2013-06-17 16:12:59 +0200

    repo

---
+++ Kwiaciarnia/Box.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kwiaciarnia
+{
+    public class Box : FlowersContainer
+    {
+        public Customer Customer { get; set; }
+
+        public Box(Customer customer)
+        {
+            Customer = customer;
+            Flowers = new List<Flower>();
+        }
+    }
+}
